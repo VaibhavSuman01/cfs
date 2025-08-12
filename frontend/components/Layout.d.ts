d9af9a0c034029ffe9e8cac8302f29417cdcc3e1
Tanvisharma31 <2004tanvisharma@gmail.com>
2025-08-12 19:23:50 +0530

    deleted:    frontend/components/AdminLayout.js deleted:    frontend/components/Footer.js deleted:    frontend/components/Header.js deleted:    frontend/components/Layout.d.ts deleted:    frontend/components/Layout.tsx deleted:    frontend/components/LoadingButton.d.ts deleted:    frontend/components/LoadingButton.js deleted:    frontend/components/LoadingState.d.ts deleted:    frontend/components/LoadingState.js deleted:    frontend/components/RunningBanner.js deleted:    frontend/components/StorageMonitor.js deleted:    frontend/components/StorageMonitorProvider.js deleted:    frontend/next.config.js modified:   frontend/package-lock.json deleted:    frontend/postcss.config.js deleted:    frontend/public/favicon.svg deleted:    frontend/public/hero-image.svg deleted:    frontend/public/logo.svg deleted:    frontend/public/logo1.png deleted:    frontend/public/rupee_icon.png new file:   frontend/styles/globals.css deleted:    frontend/tailwind.config.js deleted:    frontend/temp_page_source.html deleted:    frontend/tsconfig.tsbuildinfo deleted:    frontend/utils/apiConfig.js deleted:    frontend/utils/auth.js deleted:    frontend/utils/config.js deleted:    frontend/utils/errorHandler.js deleted:    frontend/utils/fileUtils.js deleted:    frontend/utils/httpClient.js deleted:    frontend/utils/imageOptimizer.js deleted:    frontend/utils/prefetchData.js deleted:    frontend/utils/storageManager.js deleted:    frontend/utils/storageOptimizer.js deleted:    frontend/utils/transformers.js deleted:    frontend/utils/useApiCache.js deleted:    frontend/utils/useLazyLoad.js deleted:    frontend/utils/useRenderOptimizer.js deleted:    frontend/utils/useRobustApiCache.js deleted:    frontend/utils/useSmartApiCache.js deleted:    frontend/utils/useStorageMonitor.js deleted:    frontend/utils/validationSchemas.js

--- frontend/components/Layout.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-
-export interface LayoutProps {
-  children: ReactNode;
-}
-
-declare function Layout(props: LayoutProps): React.ReactElement;
-
-export default Layout;
